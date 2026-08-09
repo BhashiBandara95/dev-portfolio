cd5be3fc3c0613185bbcbf850bbfc51078a54e34
BhashiBandara95 <ugabkanishka@gmail.com>
2026-08-10 01:05:30 +0530

    fes:(style): configure tailwind css and use variable css in index.css file, add style to the background, border, surface, etc

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"; // a helper provided by Vite.
+import react from "@vitejs/plugin-react"; // adds React support to Vite.
 import tailwindcss from "@tailwindcss/vite"; // vite project configure with tailwindcss
-import path from "path";
+import path from "path"; //  built-in module and support working with file and directory paths
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	// exports your Vite configuration so Vite can read it.(when we run npm run dev Vite reads vite.config.js and uses this configuration.)
+	plugins: [react(), tailwindcss()], // register plugins
 	resolve: {
 		alias: {
+			// alias allows you to create a shorter name for a directory
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
 });
+
+/* 
+*** Note ***
+Without the alias, you might have imports like:
+import Button from "../../../components/Button";
+With your @ alias, you can write:
+import Button from "@/components/Button";
+*/
